1310c890d1a13db5af0848aa88a51408e622f2dc
edoardo10x <54998328+edoardo10x@users.noreply.github.com>
2019-11-16 21:44:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,89 @@
 #!/usr/bin/python3
-#import telegrambot #TODO: telegrambot cannot be imported due to config missing
-from crawler import *
+import telegrambot
+from crawler import getNotifications, DATA
+from Q2A_Pi import Keys
+import time
+from datetime import datetime
+
 
 def formatMessage(notification):
-    text = ''
+    text = '''New Activity in {}
+{} {} by {}'''
     data = notification[DATA]
-    
-    creator = data[Keys.CREATED][Keys.USER]
-    editor = data[Keys.LAST_EDIT][Keys.USER]
-    creator = 'its' if creator == editor else f"{creator}'s'"
-
-    #checking data type to discover if it's a question/answer/comment
-    #CASE QUESTION
-    if(data[Keys.TYPE] == Keys.TYPE_QUESTIONS):
-        if(notification[Keys.TYPE] == NOTIFTYPE_ADD):
-            text = f'{editor} added a question: {data[Keys.TITLE]}'
-        elif(notification[Keys.TYPE] == NOTIFTYPE_EDIT):
-            text = f'{editor} edited {creator} question ({data[Keys.TITLE]})'
-
-    #CASE ANSWER
-    elif(data[Keys.TYPE] == Keys.TYPE_ANSWERS):
-        answer = data
+    nType = data[Keys.TYPE]  # Notification Type
+    # Case Question
+    if nType == Keys.TYPE_QUESTIONS:
+        question = data
+        who = data[Keys.LAST_EDIT][Keys.WHO]
+        who = f'<a href="{buildUserLink(who)}">{who}</a>'
+    # Case Answer
+    elif nType == Keys.TYPE_ANSWERS:
         question = data[Keys.PARENT]
-
-        if(notification[Keys.TYPE] == NOTIFTYPE_ADD):
-            text = f'{editor} added a answer to the question: {question[Keys.TITLE]} (from {answer[Keys.CREATED][Keys.USER]})'
-        elif(notification[Keys.TYPE] == NOTIFTYPE_EDIT):
-            text = f'{editor} edited {creator} answer to the question: {question[Keys.TITLE]} (from {answer[Keys.CREATED][Keys.USER]})'
-
-    #CASE COMMENT
-    elif(data[Keys.TYPE] == Keys.TYPE_COMMENTS):
-        comment  = data
-        answer   = comment[Keys.PARENT]
+        who = data[Keys.LAST_EDIT][Keys.WHO]
+        who = f'<a href="{buildUserLink(who)}">{who}</a>'
+    # Altrimenti è un commento
+    else:
+        answer = data[Keys.PARENT]
         question = answer[Keys.PARENT]
-        if(notification[Keys.TYPE] == NOTIFTYPE_ADD):
-            text = f'{editor} added a comment to {answer[Keys.CREATED][Keys.USER]}\'s answer to the question: {question[Keys.TITLE]}'
-        elif(notification[Keys.TYPE] == NOTIFTYPE_EDIT):
-            text = f'{editor} edited {creator} comment to {answer[Keys.CREATED][Keys.USER]}\'s answer to the question: {question[Keys.TITLE]}'
-    
+        who = data[Keys.LAST_EDIT][Keys.WHO]
+        parentWho = answer[Keys.LAST_EDIT][Keys.WHO]
+        who = f'<a href="{buildUserLink(who)}">{who}</a> on <a href="{buildUserLink(parentWho)}">{parentWho}</a>\'s answer'
+
+    title = question[Keys.TITLE]
+    questionID = question[Keys.ID]
+    title = f'<a href="{buildTitleLink(questionID)}">{title}</a>'
+    otherID = data[Keys.ID]
+    what = data[Keys.LAST_EDIT][Keys.WHAT]
+    what = f'<a href="{buildWhatLink(nType, questionID, otherID)}">{what}</a>'
+    when = getWhen(data[Keys.LAST_EDIT][Keys.WHEN])
+    text = text.format(title, what, when, who)
+
     return text
 
+
+def buildWhatLink(nType, questionID, otherID):
+    types = {Keys.TYPE_QUESTIONS: 'q',
+             Keys.TYPE_ANSWERS: 'a',
+             Keys.TYPE_COMMENTS: 'c'}
+    link = f'https://q2a.di.uniroma1.it/{questionID}/#{types[nType]}{otherID}'
+    return link
+
+
+def buildTitleLink(questionID):
+    return f'https://q2a.di.uniroma1.it/{questionID}/'
+
+
+def buildUserLink(username):
+    return f'https://q2a.di.uniroma1.it/user/{username}'
+
+
+def getWhen(timestamp):
+    '''Trasforma il timestamp nel when di q2a'''
+    FMT = '%H:%M:%S'
+    gmtime = time.strftime(FMT, time.gmtime())
+    timestamp = timestamp[11:19]
+    tdelta = datetime.strptime(gmtime, FMT) - datetime.strptime(timestamp, FMT)
+    hh, mm, ss = map(int, str(tdelta).split(':'))
+    if hh:
+        s = 's' if hh > 1 else ''
+        return f'{hh} hour{s} ago'
+    if mm:
+        s = 's' if mm > 1 else ''
+        return f'{mm} minute{s} ago'
+    s = 's' if ss > 1 else ''
+    return f'{ss} second{s} ago'
+
+
 def main():
-    #initializing telegram bot
-    #bot = telegrambot.Bot()
+    # initializing telegram bot
+    bot = telegrambot.Bot()
 
     notifications = getNotifications()
 
     for notification in notifications:
         text = formatMessage(notification)
-        print(text)
-        #for chatId in bot.users:
-        #    bot.sendMessage(chatId, text)
+        for chatId in bot.users:
+            bot.sendMessage(chatId, text)
 
 
 if __name__ == '__main__':
